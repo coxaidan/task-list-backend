51865e9f3229f364cd6cfafa55abfe164b905bc1
Aidan Cox <aidan2002@gmail.com>
2022-06-29 13:08:04 -0400

    Implement Devise API JWT

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,26 +1,26 @@
 class Users::SessionsController < Devise::SessionsController
     respond_to :json
-
+  
     private
-
-    respond_with(_resource, _opts = {})
-        render json: {
-            message: 'You are logged in.',
-            user: current_user
-        }, status: :ok
+  
+    def respond_with(_resource, _opts = {})
+      render json: {
+        message: 'You are logged in.',
+        user: current_user
+      }, status: :ok
     end
-
+  
     def respond_to_on_destroy
-        logged_out_success && return if current_user
-
-        log_out_failure
+      log_out_success && return if current_user
+  
+      log_out_failure
     end
-
-    def logged_out_success
-        render json: { message: 'You are logged out.' }, status: :ok
+  
+    def log_out_success
+      render json: { message: 'You are logged out.' }, status: :ok
     end
-
+  
     def log_out_failure
-        render json: { message: 'Hmm nothing happend.' }, status: :unauthorized
+      render json: { message: 'Hmm nothing happened.' }, status: :unauthorized
     end
-end
+  end
