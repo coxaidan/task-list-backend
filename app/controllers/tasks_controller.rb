3f943561361911810c65a756d9c63d617a860d60
Aidan Cox <aidan2002@gmail.com>
2022-07-19 15:07:27 -0400

    Created task db

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,61 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: %i[ show update destroy ]
+
+  # GET /tasks
+  def index
+    @tasks = Task.where(:user_id => get_user_from_token).order(created_at: :desc)
+  
+    render json: @tasks
+  end
+
+  # GET /tasks/1
+  def show
+    render json: @task
+  end
+
+  # POST /tasks
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      render json: @task, status: :created, location: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /tasks/1
+  def update
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /tasks/1
+  def destroy
+    @task.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def task_params
+      params.require(:task).permit(:name, :description, :user_id)
+    end
+
+    def get_user_from_token
+      headerTest = request.headers['Authorization']
+      p headerTest
+
+      jwt_payload = JWT.decode(headerTest.split(' ')[1],
+                               Rails.application.credentials.devise[:jwt_secret_key]).first
+      user_id = jwt_payload['sub']
+      User.find(user_id.to_s)
+    end
+end
