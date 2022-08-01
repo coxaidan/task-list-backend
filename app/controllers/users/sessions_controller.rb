110d07df23742fa266d8a90cc73c3b58fab9150b
Aidan Cox <aidan2002@gmail.com>
2022-08-01 19:49:19 -0400

    Added 401 error response to failed login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,10 +4,13 @@ class Users::SessionsController < Devise::SessionsController
     private
   
     def respond_with(_resource, _opts = {})
-      render json: {
-        message: 'You are logged in.',
-        user: current_user
-      }, status: :ok
+      # render json: {
+      #   message: 'You are logged in.',
+      #   user: current_user
+      # }, status: :ok
+      log_in_success && return if current_user
+  
+      log_in_failure
     end
   
     def respond_to_on_destroy
@@ -23,4 +26,13 @@ class Users::SessionsController < Devise::SessionsController
     def log_out_failure
       render json: { message: 'Hmm nothing happened.' }, status: :unauthorized
     end
+
+    def log_in_success
+      render json: { message: 'Successfully logged in.', user: current_user}, status: :ok
+    end
+
+    def log_in_failure
+      render json: { message: 'Incorrect email or password.', user: current_user}, status: :unauthorized
+    end
+
   end
