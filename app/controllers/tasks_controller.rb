675499f685c958e81fa8675db4560ae2cce7b8c8
Aidan Cox <aidan2002@gmail.com>
2023-01-01 11:58:16 -0500

    Cleanup

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,8 +52,6 @@ class TasksController < ApplicationController
     def get_user_from_token
       jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
                                Rails.application.credentials.devise[:jwt_secret_key]).first
-      puts "----------------------------------------------------------"
-      puts jwt_payload
       user_id = jwt_payload['sub']
       User.find(user_id.to_s)
     end
